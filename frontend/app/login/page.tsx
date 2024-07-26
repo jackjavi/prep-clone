ad442d83d05a745f5522987a674c6e0888c07194
jackjavi <jackmtembete@gmail.com>
2024-07-26 11:57:32 +0300

    login coming soon!

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center text-4xl h-screen">
+      LOGIN PAGE COMING SOON!!
+    </div>
+  );
+};
+
+export default page;
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        Inter: ["var(--font-inter)"],
+        mono: ["var(--font-roboto-mono)"],
+        montserrat: ["var(--font-montserrat)"],
+        playfair: ["var(--font-playfair-display)"],
+      },
     },
   },
   plugins: [],
