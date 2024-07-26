6087df94d8de8287870e26e9774553f008ce27f2
jackjavi <jackmtembete@gmail.com>
2024-07-26 09:15:19 +0300

    auth updates + .gitignore file

--- backend/app/controllers/auth/authController.mjs
+++ backend/app/controllers/auth/authController.mjs
@@ -11,7 +11,6 @@ const register = async (req, res) => {
       email,
       password,
       name,
-      profilePicture: profilePictureUrl,
       verificationToken,
     });
     await user.save();
