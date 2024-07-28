276f374f4af285ed1767634cee2d9b675781f04e
jackjavi <jackmtembete@gmail.com>
2024-07-28 17:32:03 +0300

    google meet

--- backend/app/routes/index.mjs
+++ backend/app/routes/index.mjs
@@ -1,9 +1,11 @@
 import authRoutes from "./auth/authRoutes.mjs";
 import appRoutes from "./app/appRoutes.mjs";
+import { createGoogleMeetLink } from "../services/googleMeetService.mjs";
 
 const routes = (app) => {
   app.use("/api/auth", authRoutes);
   app.use("/api/app", appRoutes);
+  app.get("/api/googleMeet", createGoogleMeetLink);
 };
 
 export default routes;
