11e8c14f84ebf35804384db3c7d725d7de7e9d71
jackjavi <jackmtembete@gmail.com>
2024-07-28 18:35:16 +0300

    meet paused

--- backend/app/routes/google/googleMeet.mjs
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createGoogleMeetLink } from "../../services/googleMeetService.mjs";
-
-const router = express.Router();
-
-router.get("/googleMeet", createGoogleMeetLink);
-
-export default router;
--- backend/app/routes/index.mjs
+++ backend/app/routes/index.mjs
@@ -1,11 +1,9 @@
 import authRoutes from "./auth/authRoutes.mjs";
 import appRoutes from "./app/appRoutes.mjs";
-import googleMeet from "./google/googleMeet.mjs";
 
 const routes = (app) => {
   app.use("/api/auth", authRoutes);
   app.use("/api/app", appRoutes);
-  app.use("/api/google", googleMeet);
 };
 
 export default routes;
