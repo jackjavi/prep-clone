73eeabae8bee421fab93ef4aa1764e2cb505b811
jackjavi <jackmtembete@gmail.com>
2024-07-31 21:22:48 +0300

    refresh

--- frontend/app/meeting-board/ScheduleMockInterview.jsx
+++ frontend/app/meeting-board/ScheduleMockInterview.jsx
@@ -65,6 +65,7 @@ const ScheduleMockInterview = () => {
     try {
       axios.post(`/api/google/gmeet`, data).then((response) => {
         alert(response.data);
+        router.refresh();
       });
     } catch (error) {
       console.error("Error scheduling mock interview:", error);
