04a60f60322b97cd58e52bfaabee2ad93e86d091
jackjavi <jackmtembete@gmail.com>
2024-07-31 19:51:45 +0300

    My account Page coming soon!!!!

---
+++ frontend/app/my-account/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../(home)/Navbar";
+
+const page = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="h-screen flex items-center justify-center">
+        COMING SOON!
+      </div>
+    </>
+  );
+};
+
+export default page;
