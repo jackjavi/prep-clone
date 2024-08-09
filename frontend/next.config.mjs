6914988dd6ff685db81541091fa8d255a84b27e0
jackjavi <jackmtembete@gmail.com>
2024-08-09 20:18:25 +0300

    disable eslint

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,10 +4,15 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        //destination: "http://localhost:5555/api/:path*",
+        // destination: "http://localhost:5555/api/:path*",
         destination: "https://prep-clone-r4qq.onrender.com/api/:path*",
       },
     ];
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
